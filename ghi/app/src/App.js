70f413985ab6e01410f7f859b2b74f7055206ad7
Jonathan Chen <jonathanchen6@gmail.com>
2022-08-01 16:54:04 -0700

    created vehicle model form and list automobiles

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
-
+import CreateVehicleModel from './CreateVehicleModel'
+import ListAutomobiles from './ListAutomobiles';
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +10,8 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/models/create" element={<CreateVehicleModel />} />
+          <Route path="/automobiles" element={<ListAutomobiles />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ ghi/app/src/CreateVehicleModel.js
@@ -0,0 +1,105 @@
+import React from 'react'
+
+class CreateVehicleModel extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            pictureUrl: '',
+            manufacturers: [],
+        }
+        this.handleName = this.handleName.bind(this);
+        this.handlePictureURL = this.handlePictureURL.bind(this);
+        this.handleManufacturerChange = this.handleManufacturerChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleName(event) {
+        const value = event.target.value;
+        this.setState({name:value});
+    }
+
+    handlePictureURL(event) {
+        const value = event.target.value;
+        this.setState({picture_url: value});
+    }
+
+    handleManufacturerChange(event) {
+        const value = event.target.value;
+        this.setState({manufacturerId: value});
+    }
+
+    async handleSubmit(event) {
+        event.preventDefault();
+        const data = {...this.state};
+        data.picture_url = data.pictureUrl
+        data.manufacturer_id = data.manufacturerId
+        delete data.pictureUrl
+        delete data.manufacturerId
+        delete data.manufacturers
+
+        const modelUrl =  'http://localhost:8100/api/models/'
+        const fetchConfig = {
+            method: "post",
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        };
+        const response = await fetch(modelUrl, fetchConfig);
+        if (response.ok) {
+            const cleared = {
+                name: '',
+                pictureUrl: '',
+                manufacturer: '',
+            }
+            this.setState(cleared)
+        }
+
+    }
+
+    async componentDidMount() {
+        const url = 'http://localhost:8100/api/manufacturers/'
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            this.setState({manufacturers: data.manufacturers})
+        }
+    }
+    render() {
+        return(
+            <div className="row">
+                <div className="offset-3 col-6">
+                    <div className="shadow p-4 mt-4">
+                        <h1>Create a Vehicle Model </h1>
+                        <form onSubmit={this.handleSubmit} id="create-vehicle-form">
+                            <div className="form-floating mb-3">
+                                <input placeholder="Name" onChange={this.handleName} required type="text" id="name" name="vehicle_name" className="form-control"/>
+                                <label htmlFor="name">Name</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input placeholder="picture_url" onChange={this.handlePictureURL} type="text" name="picture_url" id="picture_url" className="form-control"/>
+                                <label htmlFor="picture_url">Picture URL</label>
+                            </div>
+                            <div className="mb-3">
+                                <select required onChange={this.handleManufacturerChange} id="conference" name="conference" className="form-select">
+                                <option value="">Choose a manufacturer</option>
+                                {this.state.manufacturers.map(manufacturer => {
+                                    return(
+                                        <option key={manufacturer.id} value ={manufacturer.name}>
+                                            {manufacturer.name}
+                                        </option>
+                                    )
+                                })}
+                                </select>
+                            </div>
+                            <button className="btn btn-primary">Create</button>
+                        </form>
+                    </div>
+                </div>
+          </div>
+        )
+    }
+}
+
+export default CreateVehicleModel
